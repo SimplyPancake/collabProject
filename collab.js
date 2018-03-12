39888b25e37f72d1bc014a86b229ab3c1bf9ae70
SimplyPancake <danielduikvis@gmail.com>
2018-03-12 19:27:15 +0100

    Initial commit

---
+++ collab.js
@@ -0,0 +1,115 @@
+function langArabic() {
+
+}
+function langChinese() {
+
+}
+function langDutch() {
+document.getElementById("video-games").innerHTML = "Videospellen";
+document.getElementById("home").innerHTML = "Hoofdpagina"
+}
+function langEnglish() {
+
+}
+function langHindi() {
+
+}
+function langJapanese() {
+
+}
+function langUrdu() {
+
+}
+
+function search() {
+  swal({
+  text: "Search for a game eg. Minecraft",
+  closeOnClickOutside: false,
+  closeOnEsc: false,
+  content: {
+  element: "input",
+  attributes: {
+      placeholder: "Search!"
+    }
+  },
+    buttons: ["Dismiss", true],
+  }).then((gameSearch) => {
+    if (gameSearch == "Minecraft") {
+      swal({
+        closeOnClickOutside: false,
+        closeOnEsc: false,
+        title: "Minecraft",
+        text: "Here's a Documentation on Minecraft",
+        icon: "MinecraftJpg.jpg"
+      })
+    } else if (gameSearch == "Roblox") {
+      swal({
+        closeOnClickOutside: false,
+        closeOnEsc: false,
+        title: "Roblox",
+        icon: "RobloxPng.png"
+      })
+    } else  if (gameSearch == "") {
+      swal("Input=Nothing", "You have to type something!!", "warning", {
+        buttons: ["Dismiss", true]
+      }).then((inputNothing) => {
+        if (inputNothing) {
+          swal({
+          text: "Search for a game eg. Minecraft",
+          closeOnClickOutside: false,
+          closeOnEsc: false,
+          content: {
+          element: "input",
+          attributes: {
+              placeholder: "Search!"
+            }
+          },
+            buttons: ["Dismiss", true],
+          }).then((gameSearch) => {
+            if (gameSearch == "Minecraft") {
+              swal({
+                closeOnClickOutside: false,
+                closeOnEsc: false,
+                title: "Minecraft",
+                text: "Here's a Documentation on Minecraft",
+                icon: "MinecraftJpg.jpg"
+              })
+            } else if (gameSearch == "Roblox") {
+              swal({
+                closeOnClickOutside: false,
+                closeOnEsc: false,
+                title: "Roblox",
+                icon: "RobloxPng.png"
+              })
+            } else if (gameSearch == "Splatoon") {
+            swal({
+              title: "Splatoon",
+              icon
+            })
+            } else if (gameSearch == "") {
+              swal("Input=Nothing, AGAIN", "You have to type something!!", "warning", {
+                buttons: ["Dismiss", true]
+              })
+            } else {
+              swal({
+                closeOnClickOutside: false,
+                closeOnEsc: false,
+                title: "Error 404",
+                text: "Sorry, we couldn't get what you asked for!",
+                icon: "error"
+              })
+            }
+          })
+        }
+      })
+    } else {
+      swal({
+        closeOnClickOutside: false,
+        closeOnEsc: false,
+        title: "Error 404",
+        text: "Sorry, we couldn't get what you asked for!",
+        icon: "error"
+      })
+    }
+  })
+}
