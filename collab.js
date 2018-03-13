4c25be29c07d531841431c545274bc4c093a8e20
SimplyPancake <danielduikvis@gmail.com>
2018-03-13 20:09:00 +0100

    Added Overwatch game

--- collab.js
+++ collab.js
@@ -1,16 +1,18 @@
-var gameList = ["Minecraft", "Splatoon", "Roblox", "Fortnite"];
+var gameList = ["Minecraft", "Splatoon", "Roblox", "Fortnite", "Portal", "Overwatch"];
 var gameDesc = [];
 gameDesc["Minecraft"] = "Minecraft is a sandbox video game created and designed by Swedish game designer Markus 'Notch' Persson, and later fully developed and published by Mojang. The creative and building aspects of Minecraft allow players to build with a variety of different cubes in a 3D procedurally generated world. Other activities in the game include exploration, resource gathering, crafting, and combat.";
 gameDesc["Roblox"] = "Roblox is the world's largest social platform for play. Every month, over 60 million active players imagine, build, and play together within immersive 3D worlds.";
 gameDesc["Splatoon"] = "Splatoon is a third-person shooter video game developed and published by Nintendo for the Wii U, which was released worldwide in May 2015. The game centers around characters known as Inklings—beings that can transform between humanoid and squid forms, and hide or swim through colored ink sprayed on surfaces using gun, bucket, or brush-based weaponry. Splatoon features several game modes, including 4-on-4 online multiplayer, and a single player campaign.";
 gameDesc["Fortnite"] = "Fortnite is a co-op sandbox survival game developed by Epic Games and People Can Fly, with Epic Games also publishing the game. The game was released as a paid early access title for Microsoft Windows, macOS, PlayStation 4 and Xbox One on July 25, 2017, with a full free-to-play release expected in 2018.";
 gameDesc["Portal"] = "Portal is a puzzle-platform video game developed and published by Valve Corporation. It was released in a bundle package called The Orange Box for Microsoft Windows, Xbox 360 and PlayStation 3 in 2007. The game has since been ported to other systems, including OS X, Linux, and Android."
+gameDesc["Overwatch"] = "Overwatch is a team-based multiplayer online first-person shooter video game developed and published by Blizzard Entertainment. It was released in May 2016 for Windows, PlayStation 4, and Xbox One. Overwatch assigns players into two teams of six, with each player selecting from a roster of over 20 characters, known in-game as "heroes", each with a unique style of play, whose roles are divided into four general categories: Offense, Defense, Tank, and Support."
 var gameIcon = []
 gameIcon["Minecraft"] = "media/MinecraftJpg.jpg";
 gameIcon["Roblox"] = "media/RobloxPng.png";
 gameIcon["Splatoon"] = "media/SplatoonPng.png";
 gameIcon["Fortnite"] = "media/fortniteJpg.jpg";
 gameIcon["Portal"] = "media/portalJpg.jpg"
+gameIcon["Overwatch"] = "media/overwatchJpg.jpg"
 
 //Proudly edited by Pancake
 
